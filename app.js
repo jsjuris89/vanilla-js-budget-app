2433a6a625e3ce8ea757dbf42e8e8fc7f9b12ab4
Juris Poselis <jsjuris89@gmail.com>
2020-01-22 12:50:25 +0200

    initial app.js code simplified

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// This is called Module Pattern
+// https://github.com/eveningstar33/MoneyManagementApp/commit/7904c7f60aad9d8923e5c6eaff5a7d48d4f15065#diff-0364f57fbff2fabbe941ed20c328ef1a
+
 // Budget Controller
 let budgetController = (function() {
    // some code
@@ -10,19 +13,17 @@ let UIController = (function(){
 
 // Global App Controller
 let controller = (function(budgetCntrl, UICntrl) {
-   let controlAddItem = function(){
+   let addItem = function(){
       // 1. Get the field input data
       // 2. Add the item to the budget controller
       // 3. Add the new item to the UI
       // 4. Calculate the budget
       // 5. Display the budget in the UI
+      console.log('addItem function called'); 
    };
 
-   document.querySelector(".add-btn").addEventListener("click", controlAddItem);
-   document.addEventListener("keypress", function(event){
-      if (event.keyCode === 13 || event.which === 13) {
-         controlAddItem();
-      }
-   });
+   document.querySelector(".add-btn").addEventListener("click", addItem);
+   
+   
 })(budgetController, UIController)
 // Code from https://github.com/kristyburge/budget-app/blob/908ce12f63e1ed1573c72d91eb1a136872815c41/app.js
