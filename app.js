7ccde21a61edeb0528c09cfcd279179e6e796888
Juris Poselis <jsjuris89@gmail.com>
2020-01-27 21:33:55 +0200

    add if else code to get correct ID so items pushed into exp or inc arrays are in correct order

--- app.js
+++ app.js
@@ -37,13 +37,22 @@ let budgetController = (function() {
 
       // we need to create new ID => so we can utilize constructor functions for Expense or Income (since we get desc & val values already from UI)
      
-      // create new ID (basic logic) unfinished
+      // create new ID
+      //[1 2 3 4 5], next ID = 6
+      //[1 2 4 6 8], next ID = 9
+      // ID = last ID +1
       if (!data.allItems[type].length > 0) {
-         console.log("In chosen allItems.exp or allItems.inc array there is NOTHING.") 
+         // console.log("In chosen allItems.exp or allItems.inc array there is NOTHING.") 
          // set ID = 0
+         ID = 0;
          } else {
-         console.log("In chosen allItems.exp or allItems.inc array there is SOMETHING.")
+         // console.log("In chosen allItems.exp or allItems.inc array there is SOMETHING.")
          // set ID = last ID + 1
+         ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+         /* Up code destructured:
+         ID = data.allItems[inc] or ID = data.allItems[exp] + rest code [data.allItems[type].length - 1] <--- this is array element index just same as arr[0] arr[1] etc
+
+         */
          }
          
          // create new item based on 'exp' or 'inc' type
@@ -54,10 +63,12 @@ let budgetController = (function() {
          }
          // add new exp or inc to the end of the allItems.exp or allItems.inc array
          data.allItems[type].push(newItem);
+
+         return newItem;
       },
       testData: function() {
          console.log(data);
-      } 
+      }
      }
 })();
 
@@ -98,8 +109,8 @@ let controller = (function(budget, UI) {
       let newItem;
         // 1. Get the filed input data
         input = UI.getInput();
-        console.log("input values are: ", input);
-        console.log("input type is: ", input.type);
+      //   console.log("input values are: ", input);
+      //   console.log("input type is: ", input.type);
         
         // 2. Add the item to the budget controller
         newItem = budget.addItemToArray(input.type, input.description, input.value);
