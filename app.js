9044b418c78ae3f03a1df66b2d4402c8fa5252a8
Juris Poselis <jsjuris89@gmail.com>
2020-01-27 14:21:20 +0200

    created public method in budget IIFE to allow add new items to data structure  + fixed html so JS work + refactored code

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
 // Budget Controller
-
 let budgetController = (function() {
 
    // Expense function constructor
@@ -14,7 +12,7 @@ let budgetController = (function() {
       this.id = id;
       this.description = description;
       this.value = value;
-   }
+   };
 
    /* 
    The budget constructor keeps track of all the incomes and expenses and also of the budget and percentages.
@@ -30,7 +28,39 @@ let budgetController = (function() {
         inc: []
      }
   }
+
+//   Create public method to allow other modules to add new items to the data structure
+  return {
+     addItemToArray: function(type, desc, val) {
+        let newItem;
+        let ID;
+
+      // we need to create new ID => so we can utilize constructor functions for Expense or Income (since we get desc & val values already from UI)
+     
+      // create new ID (basic logic) unfinished
+      if (!data.allItems[type].length > 0) {
+         console.log("In chosen allItems.exp or allItems.inc array there is NOTHING.") 
+         // set ID = 0
+         } else {
+         console.log("In chosen allItems.exp or allItems.inc array there is SOMETHING.")
+         // set ID = last ID + 1
+         }
+         
+         // create new item based on 'exp' or 'inc' type
+         if (type === "exp") {
+            newItem = new Expense(ID, desc, val);
+         } else if (type === "inc") {
+            newItem = new Income(ID, desc, val);
+         }
+         // add new exp or inc to the end of the allItems.exp or allItems.inc array
+         data.allItems[type].push(newItem);
+      },
+      testData: function() {
+         console.log(data);
+      } 
+     }
 })();
+
 
 // UI Controller
 let UIController = (function(){
@@ -62,24 +92,24 @@ let UIController = (function(){
 let controller = (function(budget, UI) {
 
    let DOM = UI.getDOMStrings();
-   // console.log(DOM);
    
    let addItem = function() {
-      
+      let input;
+      let newItem;
         // 1. Get the filed input data
-        let input = UI.getInput();
+        input = UI.getInput();
         console.log("input values are: ", input);
+        console.log("input type is: ", input.type);
         
         // 2. Add the item to the budget controller
+        newItem = budget.addItemToArray(input.type, input.description, input.value);
         
         // 3. Add the item to the UI
-        
         // 4. Calculate the budget
-        
         // 5. Display the budget on the UI
-        console.log("Button was clicked or Enter was pressed!");
+      //   console.log("Button was clicked or Enter was pressed!");
    }
-   document.querySelector(DOM.inputBtn).addEventListener("click", addItem); // if we move this before let addItem = function()... then it wont work
+   document.querySelector(DOM.inputBtn).addEventListener("click", addItem);
    document.addEventListener('keypress', function(event){
       if (event.keyCode === 13) {
          addItem();
