fb22c64e46d1972a615316d4dfc71c040221ebf5
Juris Poselis <jsjuris89@gmail.com>
2020-01-29 20:34:09 +0200

    created custom select box for styles

--- app.js
+++ app.js
@@ -276,3 +276,19 @@ let controller = (function(budget, UI) {
    });
    document.querySelector(DOM.listsContainer).addEventListener("click", deleteItem);
 })(budgetController, UIController)
+
+// Custom Select Box Code
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+   optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+   o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+   })
+})
